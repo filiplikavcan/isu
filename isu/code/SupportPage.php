38341f9124944cdccfed1f67fa7ace567893c027
Filip Likavčan <filip@bratia.sk>
2016-01-25 10:30:39 +0100

    Add info text to support page

--- isu/code/SupportPage.php
+++ isu/code/SupportPage.php
@@ -3,7 +3,16 @@
 class SupportPage extends ArticlePage
 {
     private static $db = array(
+        'SideBarInfo' => 'HTMLText',
     );
+
+    public function getCMSFields() {
+        $fields = parent::getCMSFields();
+
+        $fields->addFieldToTab('Root.Main', new HtmlEditorField('SideBarInfo', 'SideBarInfo'), 'Content');
+
+        return $fields;
+    }
 }
 
 class SupportPage_Controller extends ArticlePage_Controller
--- themes/isu/templates/Layout/SupportPage.ss
+++ themes/isu/templates/Layout/SupportPage.ss
@@ -10,6 +10,9 @@
                 </div>
             </div>
             <div class="col-sm-5">
+
+                $SideBarInfo
+
                 <iframe src="https://docs.google.com/forms/d/1pATBSiEzQDgGC2UGEglJWpy5UjRNI6zsKXMZwEvLnCw/viewform?embedded=true" width="100%" height="700" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
             </div>
         </div>
